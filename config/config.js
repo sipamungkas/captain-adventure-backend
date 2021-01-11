7a8ab0bc7f134a7e82dfdf50e5b93c42ceeab998
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-01-12 00:08:13 +0700

    feat: added sequelize config

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'database_development',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql',
+  },
+  test: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'database_development',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'database_development',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
