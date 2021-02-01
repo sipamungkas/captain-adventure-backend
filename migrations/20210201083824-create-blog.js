aba6d1a083c9fab124f60efcbf8e7f3b7868e1f0
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-02-01 15:52:14 +0700

    feat: add blogs model, migration,seeders

---
+++ migrations/20210201083824-create-blog.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blogs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      image: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      perks: {
+        allowNull: true,
+        type: Sequelize.STRING(50),
+      },
+      short_description: {
+        allowNull: true,
+        type: Sequelize.STRING(50),
+      },
+      body: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      is_active: {
+        allowNull: false,
+        type: Sequelize.BOOLEAN,
+        defaultValue: true,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blogs');
+  },
+};
---
+++ models/blog.js
@@ -0,0 +1,29 @@
+const {Model} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Blog extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Blog.init(
+    {
+      image: DataTypes.STRING,
+      title: DataTypes.STRING,
+      perks: DataTypes.STRING,
+      short_description: DataTypes.STRING,
+      body: DataTypes.TEXT,
+    },
+    {
+      sequelize,
+      modelName: 'Blog',
+      underscored: true,
+    },
+  );
+  return Blog;
+};
