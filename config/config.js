12711bb6b763525db9ea2f9311d667766d93cd11
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-02-13 23:39:59 +0700

    test: add test for categories and new db config for test mode

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ module.exports = {
   test: {
     username: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'database_development',
+    database: `${process.env.DB_NAME}_test` || 'database_development',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
   },
--- test/index.js
+++ test/index.js
@@ -5,8 +5,9 @@ const chaiHttp = require('chai-http');
 const app = require('../app');
 
 chai.use(chaiHttp);
+let token;
 
-describe('API Test', () => {
+describe('API Test for users resources', () => {
   it('should GET /', (done) => {
     chai
       .request(app)
@@ -24,6 +25,7 @@ describe('API Test', () => {
       .post('/v1/users/session')
       .send({email: 'om.ragil7@gmail.com', password: 'password'})
       .end((err, res) => {
+        token = res.body.data.token;
         expect(err).to.be.null;
         expect(res).to.be.not.empty;
         expect(res).to.have.status(200);
@@ -38,6 +40,41 @@ describe('API Test', () => {
           'token',
           'avatar',
         );
+        done();
+      });
+  });
+});
+
+describe('API Test for categories resources', () => {
+  it('Should get categories list', (done) => {
+    chai
+      .request(app)
+      .get('/v1/categories')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.be.not.empty;
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body.meta).to.have.all.keys('message', 'code', 'status');
+        expect(res.body).to.have.property('data');
+        expect(res.body.data).to.be.an('array');
+        done();
+      });
+  });
+
+  it('should create new category, update category, delete category', (done) => {
+    chai
+      .request(app)
+      .post('/v1/categories')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        name: 'Category test',
+      })
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.be.not.empty;
+        expect(res).to.have.status(201);
         done();
       });
   });
