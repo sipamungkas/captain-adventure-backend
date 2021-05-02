420382e58cb2087b169029621b8b51b5aa2b4ed9
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-05-02 11:04:38 +0700

    feat: major update for production and testing, read description! fix: remove sequelize logging on production fix: add port on environtment variable (.env) test: make separate database between development, testing, and productino

--- config/config.js
+++ config/config.js
@@ -2,14 +2,14 @@ module.exports = {
   development: {
     username: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'database_development',
+    database: `${process.env.DB_NAME}_dev` || 'database_development',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
   },
   test: {
     username: process.env.DB_USERNAME || 'root',
     password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'database_test',
+    database: `${process.env.DB_NAME}_test` || 'database_test',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
   },
@@ -19,5 +19,6 @@ module.exports = {
     database: process.env.DB_NAME || 'database_production',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
+    logging: false,
   },
 };
