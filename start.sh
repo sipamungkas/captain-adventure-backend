791963d6e2cb4cb7ce91dfbc0987716bb697890e
Ragil Burhanudin Pamungkas <ragilburhanudinp@gmail.com>
2021-08-22 19:32:12 +0700

    prod: upgrade to mariadb 10.6 and new docker volume bind and cnf for mariadb

--- start.sh
+++ start.sh
@@ -2,33 +2,33 @@
 
 message="# Auto generated file, don't modify. Please refer to .env.dev, .env.uat or .env.prod, run $ chmod +x prepare-env.sh for permision denied\n"
 
-if [ "$1" == "production" ];
+if [ "$1" == "prod" ];
 then
   echo "Switching to Production environment"
   echo -e $message > ".env"
   cat ".env.production" >> ".env"
-  docker-compose up
+  docker-compose up -d
 
-elif [ "$1" == "staging" ]
+elif [ "$1" == "stage" ]
 then
   echo "Switching to Staging environment"
   echo -e $message > ".env"
   cat ".env.staging" >> ".env"
-  docker-compose up
+  docker-compose up -d
 
-elif [ "$1" == "development" ]
+elif [ "$1" == "dev" ]
 then
   echo "Switching to Dev environment"
   echo -e $message > ".env"
   cat ".env.development" >> ".env"
-  docker-compose up
+  docker-compose up -d
 
 elif [ "$1" == "env" ]
 then
   echo -e "ID\t\t\tPROJECT ENVIRONMENT"
-  echo -e "production\t\tproject-production"
-  echo -e "staging\t\t\tproject-staging"
-  echo -e "development\t\tproject-development"
+  echo -e "prod\t\tproject-production"
+  echo -e "stage\t\t\tproject-staging"
+  echo -e "dev\t\tproject-development"
 else
   echo "Run ‘prepare-env.sh env’ to list available environments."
 fi
--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-message="# Auto generated file, don't modify. Please refer to .env.dev, .env.uat or .env.prod, run $ chmod +x prepare-env.sh for permision denied\n"
-echo "$1" "$2"
